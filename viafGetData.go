d6b83d795b91a9c512405e0855740e70648be85d
Nicolas Morin (nicomo) <nicolas.morin@gmail.com>
2019-06-30 13:01:39 +0200

    move models for viaf into viafGetData

--- viafGetData.go
+++ viafGetData.go
@@ -10,6 +10,32 @@ import (
 
 const baseViafURL = "http://www.viaf.org/viaf/"
 
+// ViafData is used to unmarshal
+// the response coming from the
+// VIAF GetDataInFormat web service
+type ViafData struct {
+	ViafID   string  `json:"viafID"`
+	NameType string  `json:"nameType"`
+	Sources  Sources `json:"sources"`
+	XLinks   XLinks  `json:"xLinks"`
+}
+
+// Source is embedded in ViafData
+type Source struct {
+	Nsid string `json:"@nsid"`
+	Text string `json:"#text"`
+}
+
+// Sources is embedded in ViafData
+type Sources struct {
+	Source []Source `json:"source"`
+}
+
+// XLinks is embedded in ViafData
+type XLinks struct {
+	XLink string `json:"xLink"`
+}
+
 // viafGetData hits the OCLC VIAF API
 // with a VIAF ID and retrieves
 // extra information from the record
