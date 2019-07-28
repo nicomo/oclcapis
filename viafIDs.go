f8c7986efd2e95b0265c991e6c4fbfd189753ced
Nicolas Morin (nicomo) <nicolas.morin@gmail.com>
2019-07-28 09:10:18 +0200

    viaf all IDs, concurrent (batches)

--- viafIDs.go
+++ viafIDs.go
@@ -5,6 +5,15 @@ import (
 	"fmt"
 )
 
+// AllIDsResult returns the input of a request to ViafGetAllIDs
+// (e.g. 123456) and the result in the form
+// map[DNB]{"TYU6756"}, with the error if any
+type AllIDsResult struct {
+	input  string
+	output map[string]string
+	err    error
+}
+
 // ViafGetIDs finds all source IDs, e.g. LC, DNB, WKP, etc.
 // from a VIAF ID
 func ViafGetIDs(input string) (map[string]string, error) {
@@ -15,11 +24,66 @@ func ViafGetIDs(input string) (map[string]string, error) {
 	if err != nil {
 		return nil, fmt.Errorf("could not get a valid response for %s: %v", input, err)
 	}
-	fmt.Println(data)
+
 	result := map[string]string{}
 
 	for _, source := range data.SourceIDs {
 		result[source.Src] = source.SrcID
 	}
 	return result, nil
+}
+
+// ViafGetAllIDs finds all source IDs, e.g. LC, DNB, WKP, etc.
+// from a bucn of VIAF ID
+func ViafGetAllIDs(input []string) ([]AllIDsResult, error) {
+	if len(input) == 0 {
+		return nil, errors.New("input cannot be an empty string")
+	}
+
+	// 2 channels used in a fan out / fan in pattern
+	jobs := make(chan string, len(input))
+	results := make(chan AllIDsResult, len(input))
+	defer close(results)
+
+	// dispatch jobs to number of workers, capping at 5
+	numW := 5
+	if len(input) < 5 {
+		numW = len(input)
+	}
+
+	// This starts up to 5 workers, initially blocked
+	// because there are no jobs yet.
+	for w := 1; w <= numW; w++ {
+		go allIDsWorker(w, jobs, results)
+	}
+
+	// Here we send the jobs and then close the
+	// channel to indicate that's all the work we have.
+	for _, s := range input {
+		jobs <- s
+	}
+	close(jobs)
+
+	// fan in the results from the results channel
+	var res []AllIDsResult
+	for i := 1; i <= numW; i++ {
+		res = append(res, <-results)
+	}
+
+	return res, nil
+}
+
+// Here's the worker, of which we'll run several
+// concurrent instances. These workers will receive
+// work on the `jobs` channel and send the corresponding
+// results on `results`.
+func allIDsWorker(id int, jobs <-chan string, results chan<- AllIDsResult) {
+	for j := range jobs {
+		output, err := ViafGetIDs(j)
+		results <- AllIDsResult{
+			input:  j,
+			output: output,
+			err:    err,
+		}
+	}
 }
--- viafIDs_test.go
+++ viafIDs_test.go
@@ -1,37 +1,49 @@
 package oclcapis
 
 import (
+	"errors"
 	"reflect"
 	"testing"
 )
 
 type viafGetIDsTest struct {
-	Input      string
-	Expected   map[string]string
-	ShouldFail bool
+	Description string
+	Input       string
+	Expected    map[string]string
+	ShouldFail  bool
+}
+
+type ViafGetAllIDsTest struct {
+	Description string
+	Input       []string
+	Expected    []AllIDsResult
+	ShouldFail  bool
+}
+
+var viafGetIDsTests = []viafGetIDsTest{
+	{
+		Description: "JM BONNISSEAU",
+		Input:       "96731408",
+		Expected: map[string]string{
+			"SUDOC": "033522448",
+			"ISNI":  "0000000068442789",
+			"WKP":   "Q30084598",
+			"LC":    "n2009050322",
+			"BNF":   "12438130",
+			"RERO":  "vtls008694187",
+			"DNB":   "170346412",
+		},
+		ShouldFail: false,
+	},
+	{
+		Description: "empty input",
+		Input:       "",
+		Expected:    nil,
+		ShouldFail:  true,
+	},
 }
 
 func TestViafGetIDs(t *testing.T) {
-	var viafGetIDsTests = []viafGetIDsTest{
-		{
-			Input: "96731408", // JM BONNISSEAU
-			Expected: map[string]string{
-				"SUDOC": "033522448",
-				"ISNI":  "0000000068442789",
-				"WKP":   "Q30084598",
-				"LC":    "n2009050322",
-				"BNF":   "12438130",
-				"RERO":  "vtls008694187",
-				"DNB":   "170346412",
-			},
-			ShouldFail: false,
-		},
-		{
-			Input:      "",
-			Expected:   nil,
-			ShouldFail: true,
-		},
-	}
 	for _, test := range viafGetIDsTests {
 		actual, err := ViafGetIDs(test.Input)
 		if err != nil {
@@ -47,4 +59,75 @@ func TestViafGetIDs(t *testing.T) {
 			t.Fatalf("FAIL for %s: expected %v, actual result was %v", test.Input, test.Expected, actual)
 		}
 	}
+}
+
+func TestViafGetIDsBatch(t *testing.T) {
+	test := ViafGetAllIDsTest{
+		Description: "Concurrent fetching of source IDs",
+		Input: []string{
+			"96731408",
+			"",
+		},
+		Expected: []AllIDsResult{
+			AllIDsResult{
+				input: "96731408",
+				output: map[string]string{
+					"SUDOC": "033522448",
+					"ISNI":  "0000000068442789",
+					"WKP":   "Q30084598",
+					"LC":    "n2009050322",
+					"BNF":   "12438130",
+					"RERO":  "vtls008694187",
+					"DNB":   "170346412",
+				},
+				err: nil,
+			},
+			AllIDsResult{
+				input: "",
+				err:   errors.New("input cannot be an empty string"),
+			},
+		},
+		ShouldFail: false,
+	}
+
+	actual, err := ViafGetAllIDs(test.Input)
+	// general error
+	if err != nil {
+		if test.ShouldFail {
+			t.Logf("PASS %s: got expected error %v", test.Description, err)
+		} else {
+			t.Fatalf("FAIL %s (input was %s): expected %v, got error %v", test.Description, test.Input, test.Expected, err)
+		}
+	}
+
+	// checks for each specific result
+	for _, a := range actual {
+
+		// check for specific errors
+		if a.err != nil {
+			for _, e := range test.Expected {
+				if a.input == e.input {
+					if e.err != nil {
+						t.Logf("PASS %s: got expected error %v", a.input, a.err)
+						continue
+					} else {
+						t.Fatalf("FAIL %s: expected %v, got error %v", e.input, e.output, a.err)
+					}
+				}
+			}
+		}
+
+		// checks for results
+		for _, e := range test.Expected {
+			if a.input == e.input {
+				if reflect.DeepEqual(e.output, a.output) {
+					t.Logf("PASS %s", e.input)
+				} else {
+					t.Fatalf("FAIL %s: expected %v, actual result was %v", e.input, e.output, a.output)
+				}
+			}
+		}
+
+	}
+
 }
